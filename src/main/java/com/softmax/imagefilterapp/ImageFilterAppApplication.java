9e26d582947b87a83fc7877ba19490ebd54c84fe
Sanu Deokar <102058334+sanudeokar@users.noreply.github.com>
2024-09-23 13:13:11 +0530

    Add files via upload

---
+++ src/main/java/com/softmax/imagefilterapp/ImageFilterAppApplication.java
@@ -0,0 +1,13 @@
+package com.softmax.imagefilterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFilterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFilterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softmax/imagefilterapp/ImageFilterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmax.imagefilterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFilterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
